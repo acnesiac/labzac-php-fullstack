ce90e8078dec5ac119645a0dd704191f297890a8
acnesiac <acnesiac@hotmail.com>
2019-08-07 16:12:12 -0400

    ventas

---
+++ src/Conduit/Transformers/VentaTransformer.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Conduit\Transformers;
+
+use Conduit\Models\Venta;
+use League\Fractal\TransformerAbstract;
+
+class VentaTransformer extends TransformerAbstract
+{
+
+    /**
+     * @var integer|null
+     */
+    protected $requestUserId;
+
+    /**
+     * VentaTransformer constructor.
+     *
+     * @param int $requestUserId
+     */
+    public function __construct($requestUserId = null)
+    {
+        $this->requestUserId = $requestUserId;
+    }
+
+    public function transform(Venta $venta)
+    {
+        return [
+            "description"    => $venta->description,
+            'createdAt'      => $venta->created_at->toIso8601String()
+        ];
+    }
+}   
