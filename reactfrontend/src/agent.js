2bc9b46b7bbcf692b6113723d81e7ebf5f8b6bdf
acnesiac <acnesiac@hotmail.com>
2020-06-02 20:39:25 -0500

    container modified

--- reactfrontend/src/agent.js
+++ reactfrontend/src/agent.js
@@ -2,10 +2,11 @@
 import superagentPromise from 'superagent-promise';
 import _superagent from 'superagent';
 
+
 const superagent = superagentPromise(_superagent, global.Promise);
 
-//const API_ROOT = 'http://localhost/imagenesrx/public/api';
-const API_ROOT = 'http://www.laboratorioszacatelco.com.mx/api';
+const API_ROOT = 'http://localhost/imagenesrx/public/api';
+//const API_ROOT = 'http://www.laboratorioszacatelco.com.mx/api';
 
 
 const encode = encodeURIComponent;
