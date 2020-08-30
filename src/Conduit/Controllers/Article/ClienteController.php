38e4c020503ac5921ac8a768f6024684c8dab0e8
acnesiac <acnesiac@hotmail.com>
2020-08-29 23:27:39 -0500

    clientes added

--- src/Conduit/Controllers/Article/ClienteController.php
+++ src/Conduit/Controllers/Article/ClienteController.php
@@ -3,7 +3,7 @@
 namespace Conduit\Controllers\Article;
 
 use Conduit\Models\Article;
-use Conduit\Models\Venta;
+use Conduit\Models\Cliente;
 use Conduit\Models\Tag;
 use Conduit\Transformers\VentaTransformer;
 use Interop\Container\ContainerInterface;
@@ -39,58 +39,7 @@ class ClienteController
         $this->db = $container->get('db');
     }
 
-    /**
-     * Return List of Diagnosticos
-     *
-     * @param \Slim\Http\Request  $request
-     * @param \Slim\Http\Response $response
-     * @param array               $args
-     *
-     * @return \Slim\Http\Response
-     */
-    public function index(Request $request, Response $response, array $args)
-    {
-        $requestUserId = optional($requestUser = $this->auth->requestUser($request))->id;
-        $builder = Venta::query();
 
-        if ($cliente = $request->getParam('cliente')) {
-            $builder->where('cliente', $cliente);
-        }
-
-        $count = $builder->count();
-        $ventas = $builder->get();
-
-        $data = $this->fractal->createData(new Collection($ventas,
-            new VentaTransformer($requestUserId)))->toArray();
-
-        return $response->withJson(['ventas' => $data['data'], 'count' => $count])
-            ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    }
-
-    /**
-     * Return a single Venta to get article endpoint
-     *
-     * @param \Slim\Http\Request  $request
-     * @param \Slim\Http\Response $response
-     * @param array               $args
-     *
-     * @return \Slim\Http\Response
-     */
-    public function show(Request $request, Response $response, array $args)
-    {
-        $requestUserId = optional($this->auth->requestUser($request))->id;
-
-        $venta = Venta::query()->where('id', $args['id'])->firstOrFail();
-
-        $data = $this->fractal->createData(new Item($venta, new VentaTransformer($requestUserId)))->toArray();
-
-        return $response->withJson(['venta' => $data])
-			->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    }
 
     /**
      * Create and store a new Article
@@ -108,9 +57,8 @@ class ClienteController
             return $response->withJson([], 401);
         }
 
-        $this->validator->validateArray($data = $request->getParam('venta'),
+        $this->validator->validateArray($data = $request->getParam('cliente'),
             [
-
                 'cliente' => v::notEmpty()
             ]);
 
@@ -118,90 +66,17 @@ class ClienteController
             return $response->withJson(['errors' => $this->validator->getErrors()], 422);
         }
 
-        $venta = new Venta($request->getParam('venta'));
-        $venta->user_id = $requestUser->id;
-        $venta->cliente =$venta->costo;
+        $cliente = new Cliente($request->getParam('cliente'));
+        $cliente->user_id = $requestUser->id;
 
-        $venta->save();
-        $data = $this->fractal->createData(new Item($venta, new VentaTransformer()))->toArray();
+        $cliente->save();
+        $data = $this->fractal->createData(new Item($cliente, new ClienteTransformer()))->toArray();
 
-        return $response->withJson(['venta' =>$venta])
+        return $response->withJson(['venta' =>$cliente])
             ->withHeader('Access-Control-Allow-Origin', '*')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 
-    }
-
-    /**
-     * Update Article Endpoint
-     *
-     * @param \Slim\Http\Request  $request
-     * @param \Slim\Http\Response $response
-     * @param array               $args
-     *
-     * @return \Slim\Http\Response
-     */
-    public function update(Request $request, Response $response, array $args)
-    {
-        $article = Article::query()->where('slug', $args['slug'])->firstOrFail();
-        $requestUser = $this->auth->requestUser($request);
-
-        if (is_null($requestUser)) {
-            return $response->withJson([], 401);
-        }
-
-        if ($requestUser->id != $article->user_id) {
-            return $response->withJson(['message' => 'Forbidden'], 403);
-        }
-
-        $params = $request->getParam('article', []);
-
-        $article->update([
-            'title'       => isset($params['title']) ? $params['title'] : $article->title,
-            'description' => isset($params['description']) ? $params['description'] : $article->description,
-            'body'        => isset($params['body']) ? $params['body'] : $article->body,
-        ]);
-
-        if (isset($params['title'])) {
-            $article->slug = str_slug($params['title']);
-        }
-
-        $data = $this->fractal->createData(new Item($article, new DiagnosticoTransformer()))->toArray();
-
-        return $response->withJson(['article' => $data])
-            ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    }
-
-    /**
-     * Delete Article Endpoint
-     *
-     * @param \Slim\Http\Request  $request
-     * @param \Slim\Http\Response $response
-     * @param array               $args
-     *
-     * @return \Slim\Http\Response
-     */
-    public function destroy(Request $request, Response $response, array $args)
-    {
-        $article = Article::query()->where('slug', $args['slug'])->firstOrFail();
-        $requestUser = $this->auth->requestUser($request);
-
-        if (is_null($requestUser)) {
-            return $response->withJson([], 401);
-        }
-
-        if ($requestUser->id != $article->user_id) {
-            return $response->withJson(['message' => 'Forbidden'], 403);
-        }
-
-        $article->delete();
-
-        return $response->withJson([], 200)
-            ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 
 }
---
+++ src/Conduit/Transformers/ClienteTransformer.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Conduit\Transformers;
+
+use Conduit\Models\Cliente;
+use League\Fractal\TransformerAbstract;
+
+class ClienteTransformer extends TransformerAbstract
+{
+
+      /**
+       * Include resources without needing it to be requested.
+       *
+       * @var array
+       */
+      protected $defaultIncludes = [
+          'author',
+      ];
+
+      /**
+       * @var integer|null
+       */
+      protected $requestUserId;
+
+    /**
+     * VentaTransformer constructor.
+     *
+     * @param int $requestUserId
+     */
+    public function __construct($requestUserId = null)
+    {
+        $this->requestUserId = $requestUserId;
+    }
+
+    public function transform(Cliente $cliente)
+    {
+        return [
+            "id"             => $cliente->id,
+            "nombre"         => $cliente->nombre,
+            "email"          => $cliente->email,
+            "direccion"      => $cliente->direccion,
+            'createdAt'      => $cliente->created_at->toIso8601String(),
+            'updatedAt'      => isset($cliente->update_at) ? $cliente->update_at->toIso8601String() : $cliente->update_at,
+
+        ];
+    }
+
+
+
+}
