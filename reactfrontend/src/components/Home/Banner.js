8d0831654b6301b8b9c2ed9b27c65a32d17c7038
acnesiac <xenbyl@gmail.com>
2022-06-12 16:14:14 +0200

    side bar only shown when is logged user present and conditional css render (#100)
    Co-authored-by: aruizriv <adolfo.ruizrivas@asml.com>

--- reactfrontend/src/components/Home/Banner.js
+++ reactfrontend/src/components/Home/Banner.js
@@ -7,10 +7,7 @@ const Banner = ({ appName, token }) => {
   return (
     <div className="banner">
       <div className="container">
-        <h1 className="logo-font">
-          {appName.toLowerCase()}
-        </h1>
-        <p>A place to share your knowledge.</p>
+        <p>Poner aqui publicidad.</p>
       </div>
     </div>
 
