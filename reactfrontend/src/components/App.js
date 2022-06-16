3a11dadfc4fcd15d761e85aff9b6b99221710585
aruizriv <adolfo.ruizrivas@asml.com>
2022-06-16 13:51:27 +0200

    nuevo logo css sin tags

--- reactfrontend/src/components/App.js
+++ reactfrontend/src/components/App.js
@@ -4,16 +4,10 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {APP_LOAD, REDIRECT} from '../constants/actionTypes';
 import {Link, Route, Switch} from 'react-router-dom';
-import EditorVenta from '../components/EditorVenta';
 import Home from '../components/Home';
 import Login from '../components/Login';
-import Register from '../components/Register';
 import {store} from '../store';
 import {push} from 'react-router-redux';
-import EditorDiagnostico from "./EditorDiagnostico";
-import Diagnostico from '../components/Diagnostico';
-import Citas from '../components/Citas';
-import EditorCliente from "./EditorCliente";
 import Venta from "./Venta";
 
 const SideView = props => {
@@ -21,42 +15,19 @@ const SideView = props => {
     if (props.currentUser) {
         return (
             <div className="d-flex flex-column flex-shrink-0 p-3 bg-light">
-
                 <ul className="nav nav-pills flex-column mb-auto">
-
-
-                    <li className="nav-item" >
-                        <Link to="/" className="nav-link" >
+                    <li className="nav-item">
+                        <Link to="/ventas" className="nav-link">
                             <i className="ion-compose"></i>&nbsp;Ventas
                         </Link>
+
                     </li>
-
-                    <li className="nav-item" >
-                        <Link to="/clientes" className="nav-link" >
-                            <i className="ion-compose"></i>&nbsp;Clientes
-                        </Link>
-                    </li>
-
-
-                    <li className="nav-item" >
-                        <Link to="/" className="nav-link" >
-                            <i className="ion-compose"></i>&nbsp;Mis Estudios
-                        </Link>
-                    </li>
-
-
-                    <li className="nav-item" >
-                        <Link to="/citas" className="nav-link" >
-                            <i className="ion-compose"></i>&nbsp;Citas
-                        </Link>
-                    </li>
-
                 </ul>
-                </div>
-                );
-                }
-                return null;
-                };
+            </div>
+        );
+    }
+    return null;
+};
 
 const mapStateToProps = state => {
     return {
@@ -69,14 +40,11 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => ({
     onLoad: (payload, token) => {
-        //agent.Articles.all();
-        //dispatch({ type: CHANGE_TAB, payload, token, skipTracking: true });
         dispatch({type: APP_LOAD, payload, token, skipTracking: true})
     },
     onRedirect: () =>
         dispatch({type: REDIRECT})
 });
-
 
 
 class App extends React.Component {
@@ -107,21 +75,12 @@ class App extends React.Component {
                     <Header
                         appName={this.props.appName}
                         currentUser={this.props.currentUser}/>
-
-
                     <div className={this.props.currentUser ? "wrapper" : ""}>
                         <SideView currentUser={this.props.currentUser}></SideView>
-
                         <Switch>
                             <Route exact path="/" component={Home}/>
                             <Route path="/login" component={Login}/>
                             <Route path="/ventas" component={Venta}/>
-                            <Route path="/register" component={Register}/>
-                            <Route path="/editorventa" component={EditorVenta}/>
-                            <Route path="/editordiagnostico/:venta" component={EditorDiagnostico}/>
-                            <Route path="/dx/:id" component={Diagnostico}/>
-                            <Route path="/citas" component={Citas}/>
-                            <Route path="/clientes" component={EditorCliente}/>
                         </Switch>
                     </div>
                 </div>
--- reactfrontend/src/components/Venta/index.js
+++ reactfrontend/src/components/Venta/index.js
@@ -1,10 +1,10 @@
-import ArticleMeta from './ArticleMeta';
-import CommentContainer from './CommentContainer';
 import React from 'react';
 import agent from '../../agent';
 import {connect} from 'react-redux';
 import marked from 'marked';
 import {ARTICLE_PAGE_LOADED, ARTICLE_PAGE_UNLOADED} from '../../constants/actionTypes';
+import {Link} from "react-router-dom";
+import ListErrors from "../ListErrors";
 
 const mapStateToProps = state => ({
     ...state.article,
@@ -30,62 +30,19 @@ class Venta extends React.Component {
     }
 
     render() {
-        if (!this.props.article) {
-            return null;
-        }
 
-        const markup = {__html: marked(this.props.article.body, {sanitize: true})};
-        const canModify = this.props.currentUser &&
-            this.props.currentUser.username === this.props.article.author.username;
+
+
         return (
-            <div className="article-page">
-
-                <div className="banner">
-                    <div className="container">
-
-                        <h1>{this.props.article.title}</h1>
-                        <ArticleMeta
-                            article={this.props.article}
-                            canModify={canModify}/>
-                    </div>
-                </div>
-
+            <div className="auth-page">
                 <div className="container page">
+                        <div className="col-md-12">
+                            <h1 className="text-xs-center">Ingresa tu cuenta</h1>
 
-                    <div className="row article-content">
-                        <div className="col-xs-12">
 
-                            <div dangerouslySetInnerHTML={markup}></div>
-
-                            <ul className="tag-list">
-                                {
-                                    this.props.article.tagList.map(tag => {
-                                        return (
-                                            <li
-                                                className="tag-default tag-pill tag-outline"
-                                                key={tag}>
-                                                {tag}
-                                            </li>
-                                        );
-                                    })
-                                }
-                            </ul>
 
                         </div>
-                    </div>
 
-                    <hr/>
-
-                    <div className="article-actions">
-                    </div>
-
-                    <div className="row">
-                        <CommentContainer
-                            comments={this.props.comments || []}
-                            errors={this.props.commentErrors}
-                            slug={this.props.match.params.id}
-                            currentUser={this.props.currentUser}/>
-                    </div>
                 </div>
             </div>
         );
