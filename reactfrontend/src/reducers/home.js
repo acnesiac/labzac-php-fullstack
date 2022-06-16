514b06875f43976d09c1fb8f16ffa9a7dfb5db97
aruizriv <adolfo.ruizrivas@asml.com>
2022-06-16 14:18:01 +0200

    minimal issue with auth solved

--- reactfrontend/src/reducers/home.js
+++ reactfrontend/src/reducers/home.js
@@ -1,17 +1,10 @@
-import { HOME_PAGE_LOADED, HOME_PAGE_UNLOADED, SEARCH_HOME } from '../constants/actionTypes';
-import agent from '../agent';
+import { HOME_PAGE_LOADED, HOME_PAGE_UNLOADED } from '../constants/actionTypes';
 
 export default (state = {}, action) => {
   switch (action.type) {
     case HOME_PAGE_LOADED:
       return {
-        ...state,
-        tags: action.payload[0].tags
-      };
-    case SEARCH_HOME:
-      return {
-        ...state,
-        tags : []
+        ...state
       };
     case HOME_PAGE_UNLOADED:
       return {};
